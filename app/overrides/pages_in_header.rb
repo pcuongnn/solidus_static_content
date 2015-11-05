cf569f478004736a2c110550e89dbbd64794660e
pcuong <pcuongnn@gmail.com>
2015-11-05 09:41:45 +0700

    update for trim-commerce

--- app/overrides/pages_in_header.rb
+++ app/overrides/pages_in_header.rb
@@ -1,5 +1,11 @@
-Deface::Override.new(:virtual_path => "spree/shared/_main_nav_bar",
-                     :name => "pages_in_header",
-                     :insert_bottom => "#main-nav-bar",
-                     :partial => "spree/static_content/static_content_header",
-                     :disabled => false)
+# Deface::Override.new(:virtual_path => "spree/shared/_main_nav_bar",
+#                      :name => "pages_in_header",
+#                      :insert_bottom => "#main-nav-bar",
+#                      :partial => "spree/static_content/static_content_header",
+#                      :disabled => false)
+Deface::Override.new(
+  virtual_path: 'spree/shared/_main_nav_bar',
+  name: 'pages_in_header',
+  insert_bottom: '.col-xs-12 > ul:first-child',
+  partial: 'spree/static_content/static_content_header'
+)
